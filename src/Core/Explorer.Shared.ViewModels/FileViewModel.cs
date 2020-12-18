e35f02e9382565afb35d35453f3482efad76f1c1
School55 Code Team <school55codeteam@gmail.com>
2020-12-18 23:08:06 +0200

    0.1 WPF

---
+++ src/Core/Explorer.Shared.ViewModels/FileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Explorer.Shared.ViewModels
+{
+    public sealed class FileViewModel : FileEntityViewModel
+    {
+        public FileViewModel(string fileName) : base(fileName)
+        {
+            FullName = fileName;
+        }
+
+        public FileViewModel(FileInfo fileInfo) : base(fileInfo.Name)
+        {
+            FullName = fileInfo.FullName;
+        }
+    }
+}
