87e2af692b43d9e5840c12b92e479f44d2f3a041
School55 Code Team <school55codeteam@gmail.com>
2020-12-20 01:40:54 +0200

    0.2 Avalonia

---
+++ src/AvaloniaUI/Explorer.Avalonia.UI/Controls/DirectoryTabItem.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Explorer.Avalonia.UI
+{
+    public class DirectoryTabItem : UserControl
+    {
+        public DirectoryTabItem()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
