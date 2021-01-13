2829a0b0cd47af7d66b08d1addaec9fff32672c4
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-01-13 16:43:09 +0200

    0.9 Avalonia

---
+++ src/AvaloniaUI/Controls/Polcraz.GoogleChromeWindow.AvaloniaUI/Controls/ChromeTabsControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Styling;
+
+namespace Polcraz.GoogleChromeWindow.AvaloniaUI
+{
+    public class ChromeTabsControl : ListBox, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(ChromeTabsControl);
+
+
+    }
+}
