39677a9ea3fe2635af0ea8041746dc0b1dde447f
Kubariev Ilya <polcrazpolcraz@gmail.com>
2021-01-13 21:46:46 +0200

    1.1 WPF

---
+++ src/WPF/Explorer.WPF.UI/Helpers/ExtensionToImageFileConverter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Explorer.WPF.UI
+{
+    internal static class ExtensionToImageFileConverter
+    {
+        private static IconsSettings _settings;
+
+        static ExtensionToImageFileConverter()
+        {
+            var applicationDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            var iconPath = new DirectoryInfo(Path.Combine(applicationDirectory, "Icons"));
+
+            var settings = new IconsSettings();
+
+            foreach (var fileInfo in iconPath.GetFiles())
+            {
+                var name = fileInfo.Name;
+
+                var extension = name.Substring(4, name.Length - 8);
+
+                settings.Icons.Add($".{extension}", name);
+            }
+
+            IconsSettings.Save(settings, "icons.json");
+            _settings = IconsSettings.Open("icons.json");
+        }
+
+        public static FileInfo GetImagePath(string extension)
+        {
+            var applicationDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (_settings.Icons.ContainsKey(extension))
+            {
+                var path = _settings.Icons[extension];
+
+                return new FileInfo(Path.Combine(applicationDirectory, "Icons", path));
+            }
+
+            return new FileInfo(Path.Combine(applicationDirectory, "Icons", "041-3ds.svg"));
+        }
+    }
+
+    public class IconsSettings
+    {
+        public Dictionary<string, string> Icons { get; set; } = new Dictionary<string, string>();
+
+        public static IconsSettings Open(string path)
+        {
+            var json = File.ReadAllText(path);
+
+            try
+            {
+                var settings = JsonSerializer.Deserialize<IconsSettings>(json);
+                return settings;
+            }
+            catch (Exception e)
+            {
+                
+            }
+
+            return new IconsSettings();
+        }
+
+        public static void Save(IconsSettings settings, string path)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            try
+            {
+                var json = JsonSerializer.Serialize(settings, options);
+                File.WriteAllText(path, json);
+            } 
+            catch (Exception e)
+            {
+                
+            }
+
+        }
+    }
+}
--- src/WPF/Explorer.WPF.UI/ValueConverters/FileEntityToImageConverter.cs
+++ src/WPF/Explorer.WPF.UI/ValueConverters/FileEntityToImageConverter.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Explorer.Shared.ViewModels;
+using SharpVectors.Converters;
+using SharpVectors.Renderers.Wpf;
 
 namespace Explorer.WPF.UI
 {
@@ -18,9 +22,35 @@ namespace Explorer.WPF.UI
                 if (resource is ImageSource directoryImageSource)
                     return directoryImageSource;
             }
-            else if (value is FileViewModel)
+            else if (value is FileViewModel fileViewModel)
             {
+                var extension = new FileInfo(fileViewModel.FullName).Extension;
+
+                var imagePath = ExtensionToImageFileConverter.GetImagePath(extension);
+
+                if (imagePath.Extension.ToUpper() == ".SVG")
+                {
+                    var settings = new WpfDrawingSettings
+                    {
+                        TextAsGeometry = false,
+                        IncludeRuntime = true
+                    };
+
+                    var converter = new FileSvgReader(settings);
+
+                    var drawing = converter.Read(imagePath.FullName);
+
+                    if (drawing != null)
+                        return new DrawingImage(drawing);
+                }
+                else
+                {
+                    var bitmapImage = new BitmapImage(new Uri(imagePath.FullName));
+                    return bitmapImage;
+                }
+
                 var resource = Application.Current.TryFindResource("FileIconImage");
+
                 if (resource is ImageSource fileImageSource)
                     return fileImageSource;
             }
